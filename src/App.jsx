ba1ad26a11e2a791759d66489585a50546d81317
ParkourAG <guchhaitanubrata@gmail.com>
2025-06-13 22:23:17 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="background fixed w-[100vw] h-[100vh] 
-                    bg-[url('public/dark-blue-flower.jpg')]
+                    bg-[url('https://img.freepik.com/free-photo/dark-blue-wall-shadow-background-with-flower_53876-105707.jpg?t=st=1749834124~exp=1749837724~hmac=230fd4f1e7518a06498ab0e299069a8f7638a925975ee821c5d83e4cce1ce5c7&w=1380')]
                     bg-cover -z-10">
       </div>
       <Navbar />
